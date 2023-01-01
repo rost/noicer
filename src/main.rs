764e3b5c064c91884bebf7df38cf0c2dbbfecc94
Robin Osterberg <robin@repl.se>
2023-01-01 10:27:23 +0100

    Print cwd header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    print_current_dir()?;
+    Ok(())
+}
+
+fn print_current_dir() -> std::io::Result<()> {
+    let current_dir = env::current_dir()?;
+    println!("{}", current_dir.display());
+    Ok(())
 }

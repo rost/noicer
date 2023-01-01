8f937e7ceaf730531b134aa9323907947c533d20
Robin Osterberg <robin@repl.se>
2023-01-01 15:33:28 +0100

    Wrap lines in interactive crossterm example

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,91 @@
-use std::env;
-use std::io::Result;
+use std::io::{self, Write};
+
+pub use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command, Result,
+};
+
+fn run<W>(w: &mut W) -> Result<()>
+where
+    W: Write,
+{
+    execute!(w, terminal::EnterAlternateScreen)?;
+
+    terminal::enable_raw_mode()?;
+
+    loop {
+        queue!(
+            w,
+            style::ResetColor,
+            terminal::Clear(ClearType::All),
+            cursor::Hide,
+            cursor::MoveTo(1, 1)
+        )?;
+
+        for line in screen_lines() {
+            queue!(w, style::Print(line), cursor::MoveToNextLine(1))?;
+        }
+
+        w.flush()?;
+
+        match read_char()? {
+            // '1' => test::cursor::run(w)?,
+            // '2' => test::color::run(w)?,
+            // '3' => test::attribute::run(w)?,
+            // '4' => test::event::run(w)?,
+            'q' => break,
+            _ => {}
+        };
+    }
+
+    execute!(
+        w,
+        style::ResetColor,
+        cursor::Show,
+        terminal::LeaveAlternateScreen
+    )?;
+
+    terminal::disable_raw_mode()
+}
+
+pub fn read_char() -> Result<char> {
+    loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Char(c),
+            ..
+        })) = event::read()
+        {
+            return Ok(c);
+        }
+    }
+}
+
+pub fn buffer_size() -> Result<(u16, u16)> {
+    terminal::size()
+}
 
 fn main() -> Result<()> {
-    print_current_dir()?;
-    println!("");
-    print_current_dir_content()?;
-    Ok(())
+    let mut stdout = io::stdout();
+    run(&mut stdout)
 }
 
-fn print_current_dir() -> Result<()> {
-    let current_dir = env::current_dir()?;
-    println!("{}", current_dir.display());
-    Ok(())
-}
-
-fn print_current_dir_content() -> Result<()> {
-    for entry in std::fs::read_dir(".")? {
-        let entry = entry?;
+fn screen_lines() -> Vec<String> {
+    let mut lines = Vec::new();
+    let current_dir = std::env::current_dir().unwrap();
+    lines.push(format!("{}", current_dir.display()));
+    lines.push(String::from(""));
+    for entry in std::fs::read_dir(".").unwrap() {
+        let entry = entry.unwrap();
         let path = entry.path();
         let dir = path.file_name().unwrap().to_str().unwrap();
         if path.is_dir() {
-            println!("   {}/", dir);
+            lines.push(format!("   {}/", dir));
         } else {
-            println!("   {}", dir);
+            lines.push(format!("   {}", dir));
         }
     }
-    Ok(())
+    lines
 }

65bd16db0abff9055989e2ec549a7f1f405c64d7
Robin Osterberg <robin@repl.se>
2023-02-12 12:53:46 +0100

    Make types, methods and functions more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -20,35 +20,123 @@ enum OpKind {
 
 struct Op {
     kind: OpKind,
-    path: Option<PathBuf>,
+    path: PathBuf,
 }
 
 impl Op {
     fn new(kind: OpKind, path: PathBuf) -> Op {
-        Op {
-            kind,
-            path: Some(path),
-        }
+        Op { kind, path }
     }
 }
 
-struct State {
-    cursor: i32,
+struct Cursor {
     dir: PathBuf,
     paths: HashMap<PathBuf, i32>,
+    point: i32,
     prev_op: Option<Op>,
-    screen_lines: Vec<String>,
+    siblings: Vec<PathBuf>,
 }
 
-impl State {
-    fn new() -> Result<State> {
-        Ok(State {
-            cursor: 0,
+impl Cursor {
+    fn new() -> Result<Cursor> {
+        Ok(Cursor {
             dir: std::env::current_dir()?,
             paths: HashMap::new(),
+            point: 0,
             prev_op: None,
-            screen_lines: format_screen_lines(0, get_dir_content()?)?,
+            siblings: Vec::new(),
         })
+    }
+
+    fn pos(&self) -> i32 {
+        self.point
+    }
+
+    fn update_dir(&mut self) -> Result<()> {
+        self.dir = std::env::current_dir()?;
+        Ok(())
+    }
+
+    fn update_dir_content(&mut self) -> Result<()> {
+        let mut entries = Vec::new();
+        for entry in std::fs::read_dir(".")? {
+            entries.push(entry?.path());
+        }
+        entries.sort();
+        self.siblings = entries;
+        Ok(())
+    }
+
+    fn update_pos(&mut self) -> Result<()> {
+        let path_pos = self.paths.get(&self.dir);
+        let prev = self.prev_op.as_ref().map(|op| op.kind == OpKind::Out);
+        let last = self.prev_op.as_ref().and_then(|op| op.path.file_name());
+        let pos = match (path_pos, prev, last) {
+            (Some(&cursor), _, _) => cursor,
+            (None, Some(true), Some(last)) => {
+                let index = self
+                    .siblings
+                    .iter()
+                    .position(|p| p.file_name() == Some(last))
+                    .unwrap_or(0);
+                index as i32
+            }
+            _ => 0,
+        };
+        self.point = pos;
+        Ok(())
+    }
+
+    fn move_down(&mut self) -> Result<()> {
+        if self.point + 1 < self.siblings.len() as i32 {
+            self.point += 1;
+        }
+        self.prev_op = None;
+        Ok(())
+    }
+
+    fn move_up(&mut self) -> Result<()> {
+        if self.point > 0 {
+            self.point -= 1;
+        } else {
+            self.point = 0;
+        }
+        self.prev_op = None;
+        Ok(())
+    }
+
+    fn move_out_of_dir(&mut self) -> Result<()> {
+        std::env::set_current_dir("..")?;
+        let op = Some(Op::new(OpKind::Out, self.dir.clone()));
+        self.point = self.pos();
+        self.prev_op = op;
+        Ok(())
+    }
+
+    fn move_into_dir(&mut self) -> Result<()> {
+        if !self.siblings.is_empty() {
+            let path = &self.siblings[(self.pos()) as usize];
+            let file = path.file_name().unwrap_or(OsStr::new(""));
+            let newdir = self.dir.join(file);
+            if newdir.is_dir() {
+                std::env::set_current_dir(newdir)?;
+            }
+        }
+        self.point = self.pos();
+        self.prev_op = None;
+        Ok(())
+    }
+
+    fn before(&mut self) -> Result<()> {
+        self.update_dir()?;
+        self.update_dir_content()?;
+        self.update_pos()?;
+        Ok(())
+    }
+
+    fn after(&mut self) -> Result<()> {
+        self.paths.insert(self.dir.clone(), self.pos());
+        Ok(())
     }
 }
 
@@ -66,7 +154,7 @@ where
 
     terminal::enable_raw_mode()?;
 
-    let mut state = State::new()?;
+    let mut cursor = Cursor::new()?;
 
     loop {
         queue!(
@@ -77,13 +165,10 @@ where
             cursor::MoveTo(1, 1)
         )?;
 
-        state.dir = std::env::current_dir()?;
+        cursor.before()?;
 
-        state.cursor = cursor_position(&state)?;
-
-        state.screen_lines = format_screen_lines(state.cursor, get_dir_content()?)?;
-
-        for line in &state.screen_lines {
+        let screen_lines = format_lines(&cursor)?;
+        for line in screen_lines {
             queue!(w, style::Print(line), cursor::MoveToNextLine(1))?;
         }
 
@@ -91,10 +176,10 @@ where
 
         match read_char()? {
             'q' => break,
-            char => handle_keypress(&char, &mut state)?,
+            char => handle_keypress(&char, &mut cursor)?,
         };
 
-        state.paths.insert(state.dir, state.cursor);
+        cursor.after()?;
     }
 
     execute!(
@@ -107,107 +192,40 @@ where
     Ok(terminal::disable_raw_mode()?)
 }
 
-fn cursor_position(state: &State) -> Result<i32> {
-    let cursor = if state.paths.contains_key(&state.dir) {
-        match state.paths.get(&state.dir) {
-            Some(cursor) => *cursor,
-            None => 0,
-        }
+fn format_lines(cursor: &Cursor) -> Result<Vec<String>> {
+    let empty_dir = vec![PathBuf::from("   ../")];
+    let content = if !cursor.siblings.is_empty() {
+        &cursor.siblings
     } else {
-        match &state.prev_op {
-            Some(op) if op.kind == OpKind::Out => {
-                let last = match op.path.as_ref() {
-                    Some(path) => match path.file_name() {
-                        Some(v) => v,
-                        None => OsStr::new(""),
-                    },
-                    None => OsStr::new(""),
-                };
-                let index = get_dir_content()?
-                    .iter()
-                    .position(|x| x.file_name() == Some(last))
-                    .unwrap_or(0);
-                index as i32
-            }
-            Some(_) => 0,
-            None => 0,
-        }
-    };
-    Ok(cursor)
-}
-
-fn handle_keypress<'a>(char: &char, state: &'a mut State) -> Result<&'a mut State> {
-    let state = match char {
-        'j' => {
-            state.cursor = move_down(state)?;
-            state.prev_op = None;
-            state
-        }
-        'k' => {
-            state.cursor = move_up(state)?;
-            state.prev_op = None;
-            state
-        }
-        'h' => {
-            let cursor = move_out_of_dir(state)?;
-            let op = Some(Op::new(OpKind::Out, state.dir.clone()));
-            state.cursor = cursor;
-            state.prev_op = op;
-            state
-        }
-        'l' => {
-            state.cursor = move_into_dir(state)?;
-            state.prev_op = None;
-            state
-        }
-        _ => state,
-    };
-    Ok(state)
-}
-
-fn get_dir_content() -> Result<Vec<PathBuf>> {
-    let mut entries = Vec::new();
-    for entry in std::fs::read_dir(".")? {
-        let entry = entry?;
-        let path = entry.path();
-        entries.push(path);
-    }
-    entries.sort();
-    Ok(entries)
-}
-
-fn format_screen_lines(cursor: i32, content: Vec<PathBuf>) -> Result<Vec<String>> {
-    let content = match !content.is_empty() {
-        true => content,
-        false => vec![PathBuf::from("   ../")],
+        &empty_dir
     };
 
     let mut lines = Vec::new();
-    let current_dir = std::env::current_dir()?;
-    lines.push(format!("{}", current_dir.display()));
+    lines.push(format!("{}", cursor.dir.display()));
     lines.push(String::from(""));
 
     for entry in content {
-        lines.push(pathbuf_to_string(&entry));
+        lines.push(format_pathbuf(entry)?);
     }
 
-    let index = (cursor + 2) as usize;
+    let index = (cursor.pos() + 2) as usize;
     lines[index] = format!(" > {}", lines[index].trim_start());
 
     Ok(lines)
 }
 
-fn pathbuf_to_string(path: &Path) -> String {
-    match path.file_name() {
-        Some(v) => match v.to_str() {
-            Some(v) => match path.is_dir() {
-                true => format!("   {v}/"),
-                false => format!("   {v}"),
-            },
-            None => "".to_string(),
-        },
-        None => "".to_string(),
-    }
+fn format_pathbuf(path: &Path) -> Result<String> {
+    let f = path.file_name();
+    let s = match f {
+        Some(v) => v.to_str(),
+        None => None,
+    };
+    let r = match (path.is_dir(), s) {
+        (true, Some(v)) => format!("   {v}/"),
+        (false, Some(v)) => format!("   {v}"),
+        _ => String::from(""),
+    };
+    Ok(r)
 }
 
 fn read_char() -> Result<char> {
@@ -222,38 +240,13 @@ fn read_char() -> Result<char> {
     }
 }
 
-fn move_down(state: &State) -> Result<i32> {
-    let cursor = if state.cursor + 1 < (state.screen_lines.len() - 2) as i32 {
-        state.cursor + 1
-    } else {
-        state.cursor
+fn handle_keypress(char: &char, arrow: &mut Cursor) -> Result<()> {
+    match char {
+        'j' => arrow.move_down()?,
+        'k' => arrow.move_up()?,
+        'h' => arrow.move_out_of_dir()?,
+        'l' => arrow.move_into_dir()?,
+        _ => (),
     };
-    Ok(cursor)
-}
-
-fn move_up(state: &State) -> Result<i32> {
-    let cursor = if state.cursor > 0 {
-        state.cursor - 1
-    } else {
-        0
-    };
-    Ok(cursor)
-}
-
-fn move_out_of_dir(state: &State) -> Result<i32> {
-    std::env::set_current_dir("..")?;
-    Ok(state.cursor)
-}
-
-fn move_into_dir(state: &State) -> Result<i32> {
-    let path = state.screen_lines[(state.cursor + 2) as usize].trim_start();
-    let newdir = path.trim_end_matches('/');
-    let newdir = str::replace(newdir, ">", " ");
-    let newdir = newdir.trim_start();
-    let current_dir = std::env::current_dir()?;
-    let newdir = current_dir.join(newdir);
-    if path.ends_with('/') {
-        std::env::set_current_dir(newdir)?;
-    }
-    Ok(state.cursor)
+    Ok(())
 }
